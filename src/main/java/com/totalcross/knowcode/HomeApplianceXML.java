35f4fca356aef2afe3a4c465398d4de41d23cb7f
Bruno Muniz <brunoamuniz@gmail.com>
2020-05-11 15:47:12 -0300

    first commit!

---
+++ src/main/java/com/totalcross/knowcode/HomeApplianceXML.java
@@ -0,0 +1,79 @@
+
+package com.totalcross.knowcode;
+
+import com.totalcross.knowcode.parse.XmlScreenAbstractLayout;
+import com.totalcross.knowcode.parse.XmlScreenFactory;
+
+import totalcross.sys.Convert;
+import totalcross.sys.InvalidNumberException;
+import totalcross.sys.Settings;
+import totalcross.ui.Button;
+import totalcross.ui.Label;
+import totalcross.ui.MainWindow;
+import totalcross.ui.event.ControlEvent;
+import totalcross.ui.event.PressListener;
+
+public class HomeApplianceXML extends MainWindow {
+
+	public HomeApplianceXML() {
+		setUIStyle(Settings.MATERIAL_UI);
+	}
+
+	static {
+		Settings.applicationId = "TCMT";
+		Settings.appVersion = "1.0.0";
+		Settings.iosCFBundleIdentifier = "com.totalcross.easytiful";
+	}
+
+	public void initUI() {
+
+		XmlScreenAbstractLayout xmlCont = (XmlScreenAbstractLayout) XmlScreenFactory.create("xml/homeApplianceXML.xml");
+		swap(xmlCont);
+
+		Button plus = (Button) xmlCont.getControlByID("@+id/plus");
+		Label insideTempLabel = (Label) xmlCont.getControlByID("@+id/insideTempLabel");
+
+		plus.addPressListener(new PressListener() {
+
+			@Override
+			public void controlPressed(ControlEvent e) {
+				// TODO
+
+				try {
+
+					String tempString = insideTempLabel.getText();
+					int temp;
+					temp = Convert.toInt(tempString);
+					insideTempLabel.setText(Convert.toString(++temp));
+
+				} catch (InvalidNumberException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		});
+
+		Button minus = (Button) xmlCont.getControlByID("@+id/minus");
+		minus.addPressListener(new PressListener() {
+
+			@Override
+			public void controlPressed(ControlEvent e) {
+				// TODO
+
+				try {
+					String tempString = insideTempLabel.getText();
+					int temp;
+					temp = Convert.toInt(tempString);
+					insideTempLabel.setText(Convert.toString(--temp));
+
+				} catch (InvalidNumberException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		});
+
+	}
+}
---
+++ src/main/java/com/totalcross/knowcode/HomeApplianceXMLApplication.java
@@ -0,0 +1,13 @@
+
+package com.totalcross.knowcode;
+
+import totalcross.TotalCrossApplication;
+
+public class HomeApplianceXMLApplication {
+
+    public static void main(String[] args) {
+        // TotalCrossApplication.run(Knowcode.class);
+
+        TotalCrossApplication.run(HomeApplianceXML.class, "/r", "5443444B43554F0ADBF5E97E", "/scr", "848x480");
+    }
+}
